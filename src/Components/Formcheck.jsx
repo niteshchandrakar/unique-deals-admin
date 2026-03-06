53bc3646982d125eede83dc61753afab77f1d735
Nitesh <niteshchandrakar2@gmail.com>
2026-03-06 18:17:38 +0530

    seller given added to akku

--- src/Components/Formcheck.jsx
+++ src/Components/Formcheck.jsx
@@ -2,14 +2,7 @@ import { useEffect, useState } from "react";
 import { gapi } from "gapi-script";
 import dayjs from "dayjs";
 import med from "../med";
-const CLIENT_ID =
-  "937228397336-i07jo81e4e8os777rel1594n369ohnuk.apps.googleusercontent.com";
-const API_KEY = "AIzaSyDScP5GlWBV1kA8k0cfLK6r7JvRHRqqOJU";
-const SHEET_ID = "1L9LJEj43C54zbd5AJ3HW_ETt0KW1JK6sIh6-jkQSLWQ";
-const DISCOVERY_DOC =
-  "https://sheets.googleapis.com/$discovery/rest?version=v4";
-const SCOPE = "https://www.googleapis.com/auth/spreadsheets";
-
+import { CLIENT_ID, API_KEY, SHEET_ID, DISCOVERY_DOC, SCOPE } from "../config";
 function Formcheck() {
   const [mediator, setMediator] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const CLIENT_ID =
+  "937228397336-i07jo81e4e8os777rel1594n369ohnuk.apps.googleusercontent.com";
+export const API_KEY = "AIzaSyDScP5GlWBV1kA8k0cfLK6r7JvRHRqqOJU";
+export const SHEET_ID = "1L9LJEj43C54zbd5AJ3HW_ETt0KW1JK6sIh6-jkQSLWQ";
+export const DISCOVERY_DOC =
+  "https://sheets.googleapis.com/$discovery/rest?version=v4";
+export const SCOPE = "https://www.googleapis.com/auth/spreadsheets";
